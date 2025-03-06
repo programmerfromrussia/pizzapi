5e5e0a09b0d976559a47cfa4a66559ba7a4842a2
programmerfromrussia <programmerfromrussia@gmail.com>
2025-03-06 17:23:34 +0300

    Updated ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
-use Illuminate\Http\Request;
-use Validator;
 
 class ProductController extends Controller
 {
@@ -30,46 +28,5 @@ class ProductController extends Controller
         }
         return response()->json(['message' => 'Product not found'], 404);
 
-    }
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Product $product, $id)
-    {
-        $product = Product::find($id);
-
-        if (!$product) {
-            return response()->json(['message' => 'Product not found'], 404);
-        }
-
-        $validation = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'category' => 'required|string|max:100',
-            'price' => 'required|numeric|min:0',
-            'image' => 'nullable|string',
-            'is_available' => 'boolean',
-        ]);
-
-        if ($validation->fails()) {
-            return response()->json(data: $validation->errors(), status: 403);
-        }
-
-        $product->update($request->all());
-        return response()->json(data: $product, status: 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        $product = Product::find($id);
-        if(!$product){
-            return response()->json(['message' => 'Product not found'], 404);
-        }
-
-        $product->delete();
-        return response()->json(['message' => 'Product deleted successfully'], status: 200);
     }
 }
