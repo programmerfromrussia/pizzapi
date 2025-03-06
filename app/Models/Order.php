bfff6bcffc6362bd239ba9f08d492e0b4cf9029b
programmerfromrussia <programmerfromrussia@gmail.com>
2025-03-06 17:23:17 +0300

    Updated Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use App\Enums\OrderStatus;
+use App\Models\Cart;
+use App\Models\Location;
+use App\Models\OrderItem;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
